c2275d0889e6fd1f947680503ae94493fe749436
Skmooo <magarsitaram222@gmail.com>
2026-04-17 21:24:23 +0545

    app.py

--- app.py
+++ app.py
@@ -61,8 +61,7 @@ The user can trigger these by typing the number or name:
 
 """
 
-model = genai.GenerativeModel('gemini-1.5-pro', system_instruction=instruction)
-
+model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=instruction)
 # Chat logic
 if "messages" not in st.session_state:
     st.session_state.messages = []
